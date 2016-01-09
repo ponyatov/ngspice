3f2e0edd457034936b5a5ea6bffe4c073f8030f8
h_vogt <h_vogt>
2016-01-09 09:40:07 +0100

    spicenum.c: numparam error: error message, then exit

--- src/frontend/numparam/spicenum.c
+++ src/frontend/numparam/spicenum.c
@@ -497,26 +497,16 @@ nupa_done(void)
        simulation has finished. */
 
     if (nerrors) {
-
-#ifdef SHARED_MODULE
-        fprintf(cp_err, "Numparam expansion errors: Problem with input file.\n");
-        shared_exit(EXIT_BAD);
-#endif
-
-        printf(" Copies=%d Evals=%d Placeholders=%ld Symbols=%d Errors=%d\n",
+        /* numparam errors are serious, continuing does not make sense */
+        /* fixme Is this error message useful ? */
+        fprintf(cp_err, " Copies=%d Evals=%d Placeholders=%ld Symbols=%d Errors=%d\n",
                linecountS, evalcountS, placeholder, dictsize, nerrors);
-        /* debug: ask if spice run really wanted */
-        if (ft_batchmode)
-            controlled_exit(EXIT_FAILURE);
-        for (;;) {
-            int c;
-            printf("Numparam expansion errors: Run Spice anyway? y/n ?\n");
-            c = yes_or_no();    /* fixme, nupa_done, io */
-            if (c == 'n' || c == EOF)
-                controlled_exit(EXIT_FAILURE);
-            if (c == 'y')
-                break;
-        }
+        fprintf(cp_err, "Numparam expansion errors: Problem with input file.\n");
+#ifdef SHARED_MODULE
+        shared_exit(EXIT_FAILURE);
+#else
+        controlled_exit(EXIT_FAILURE);
+#endif
     }
 
     linecountS = 0;
