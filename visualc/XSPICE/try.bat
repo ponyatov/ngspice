23bb3912825ab2b38bd62dc017914a15c8c20f31
rlar <rlar>
2016-01-23 21:56:46 +0100

    upgrade try.bat

--- visualc/XSPICE/try.bat
+++ visualc/XSPICE/try.bat
@@ -8,11 +8,7 @@ set CMPP_ODIR=icm/%sub%
 if not exist icm\%sub% mkdir icm\%sub%
 .\bin\cmpp -lst
 
-for %%n in (
-  divide d_dt gain hyst ilimit int limit
-  mult multi_input_pwl oneshot pwl sine slew
-  square summer s_xfer triangle file_source
-) do (
+for /F %%n in (..\..\src\xspice\icm\%sub%\modpath.lst) do (
   set CMPP_IDIR=../../src/xspice/icm/%sub%/%%n
   set CMPP_ODIR=icm/%sub%/%%n
   if not exist icm\%sub%\%%n mkdir icm\%sub%\%%n
