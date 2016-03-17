cf943d25aaed5fddbe2c28ccc73556472d88d191
rlar <rlar>
2016-03-17 19:09:03 +0100

    frontend/inp.c, inp_parse_temper(), rewrite linked list insertion

--- src/frontend/inp.c
+++ src/frontend/inp.c
@@ -1474,7 +1474,6 @@ inp_parse_temper(struct line *card)
                 for (str_ptr = beg_pstr; str_ptr < end_tstr; str_ptr++)
                     *str_ptr = ' ';
 
-                modtlistnew->next = NULL;
                 /* create wordlist suitable for com_altermod */
                 wl_append_word(&wl, &wlend, devmodname);
                 wl_append_word(&wl, &wlend, paramname);
@@ -1485,13 +1484,8 @@ inp_parse_temper(struct line *card)
                 modtlistnew->wlend = wlend;
 
                 /* fill in the linked parse tree list */
-                if (modtlist) {
-                    struct pt_temper *modtlisttmp = modtlist;
-                    modtlist = modtlistnew;
-                    modtlist->next = modtlisttmp;
-                } else {
-                    modtlist = modtlistnew;
-                }
+                modtlistnew->next = modtlist;
+                modtlist = modtlistnew;
             }
         } else { /* instance expression with 'temper' */
             struct pt_temper *devtlistnew = NULL;
@@ -1529,7 +1523,6 @@ inp_parse_temper(struct line *card)
                 for (str_ptr = beg_pstr; str_ptr < end_tstr; str_ptr++)
                     *str_ptr = ' ';
 
-                devtlistnew->next = NULL;
                 /* create wordlist suitable for com_altermod */
                 wl_append_word(&wl, &wlend, devmodname);
                 wl_append_word(&wl, &wlend, paramname);
@@ -1540,13 +1533,8 @@ inp_parse_temper(struct line *card)
                 devtlistnew->wlend = wlend;
 
                 /* fill in the linked parse tree list */
-                if (devtlist) {
-                    struct pt_temper *devtlisttmp = devtlist;
-                    devtlist = devtlistnew;
-                    devtlist->next = devtlisttmp;
-                } else {
-                    devtlist = devtlistnew;
-                }
+                devtlistnew->next = devtlist;
+                devtlist = devtlistnew;
             }
         }
     }
