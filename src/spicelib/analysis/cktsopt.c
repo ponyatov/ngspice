c8ac62713c6dfc5a8e1aeddb4222fbc83d9fc843
rlar <rlar>
2016-04-05 21:27:06 +0200

    FIXME, for commit "nicomcof: add variable xmuext to ..."
    the variable is called "xmu" instead of "xmuext" and incorporated
      in a way which is comparable with "maxord" and "method"
    renamed for no particular reason, except for similiarity with "maxord" etc.
    
    perhaps ".option xmu" and "option xmu" work now too.
    
    most important, the rpeated and expensive calls to cp_getvar in nicomcof
      have been removed.
    
    practically untested commit
      expecially the "save"/restart/restore aspect is completely untested
      and the "report" is completely untested

--- src/spicelib/analysis/cktsopt.c
+++ src/spicelib/analysis/cktsopt.c
@@ -112,6 +112,10 @@ CKTsetOpt(CKTcircuit *ckt, JOB *anal, int opt, IFvalue *val)
     case OPT_BYPASS:
         task->TSKbypass = val->iValue;
         break;
+    case OPT_XMU:
+        fprintf(stderr, "CKTsetOpt, OPT_XMU = %g\n", val->rValue);
+        task->TSKxmu = val->rValue;
+        break;
     case OPT_MAXORD:
         task->TSKmaxOrder = val->iValue;
         /* Check options method and maxorder for consistency */
@@ -263,6 +267,7 @@ static IFparm OPTtbl[] = {
  { "lvltim", 0, IF_INTEGER,"Type of timestep control" },
  { "method", OPT_METHOD, IF_SET|IF_STRING,"Integration method" },
  { "maxord", OPT_MAXORD, IF_SET|IF_INTEGER,"Maximum integration order" },
+ { "xmu", OPT_XMU, IF_SET|IF_REAL,"Trapezoidal method, order=2, xmu coefficient" },
  { "defm", OPT_DEFM,IF_SET|IF_REAL,"Default MOSfet Multiplier" },
  { "defl", OPT_DEFL,IF_SET|IF_REAL,"Default MOSfet length" },
  { "defw", OPT_DEFW,IF_SET|IF_REAL,"Default MOSfet width" },
