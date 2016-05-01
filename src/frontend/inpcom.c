98906d769e84b0ab37babeefcc812ea13f802dac
h_vogt <h_vogt>
2016-05-01 21:18:40 +0200

    inpcom.c, add function inp_rem_unused_models()

--- src/frontend/inpcom.c
+++ src/frontend/inpcom.c
@@ -153,6 +153,7 @@ static void inp_add_levels(struct line *deck);
 bool inp_check_scope_mod(unsigned short elem_level[], unsigned short mod_level[]);
 bool inp_check_scope_sub(unsigned short x_level[], unsigned short subckt_level[]);
 static char inp_get_elem_ident(char *type);
+static void inp_rem_unused_models(struct line *deck);
 
 struct inp_read_t
 { struct line *cc;
@@ -519,6 +520,7 @@ inp_readall(FILE *fp, char *dir_name, bool comfile, bool intfile)
 
         inp_remove_excess_ws(working);
 
+        inp_rem_unused_models(working);
         comment_out_unused_subckt_models(working);
 
         subckt_params_to_param(working);
@@ -6751,9 +6753,9 @@ bool inp_check_scope_mod(unsigned short elem_level[], unsigned short mod_level[]
         return TRUE;
     /* model at nesting level */
     for (i = 0; i < NESTINGDEPTH - 1; i++)
-        if ((elem_level[i] == mod_level[i]) && (mod_level[i + 1] == 0))
+        if ((elem_level[i] > 0) && (elem_level[i] == mod_level[i]) && (mod_level[i + 1] == 0))
             return TRUE;
-    if (elem_level[NESTINGDEPTH - 1] == mod_level[NESTINGDEPTH - 1])
+    if ((elem_level[NESTINGDEPTH - 1] > 0) && (elem_level[NESTINGDEPTH - 1] == mod_level[NESTINGDEPTH - 1]))
         return TRUE;
     return FALSE;
 }
@@ -6813,4 +6815,79 @@ inp_get_elem_ident(char *type)
     /* xspice code models do not have unique type names */
     else
         return 'a';
+}
+
+/* scan through deck. If .model is found, check if elements
+   referring to that model are available. If not, comment out
+   the model line. */
+static void
+inp_rem_unused_models(struct line *deck)
+{
+    struct line *card, *card_elem;
+    int skip_control = 0, subs = 0, i;
+    static unsigned short levelinfo[NESTINGDEPTH];
+
+    for (card = deck; card; card = card->li_next) {
+
+        char *curr_line = card->li_line;
+
+        /* exclude any command inside .control ... .endc */
+        if (ciprefix(".control", curr_line)) {
+            skip_control++;
+            continue;
+        }
+        else if (ciprefix(".endc", curr_line)) {
+            skip_control--;
+            continue;
+        }
+        else if (skip_control > 0) {
+            continue;
+        }
+
+        if (*curr_line == '*')
+            continue;
+
+        if (ciprefix(".model", curr_line)) {
+            int skip_control2 = 0;
+            bool model_is_in_use = FALSE;
+            /*find model type*/
+            char *model_type = get_model_type(curr_line);
+            char *model_name = get_subckt_model_name(curr_line);
+            char elem_ident = inp_get_elem_ident(model_type);
+            for (card_elem = deck; card_elem; card_elem = card_elem->li_next) {
+                char *elem_line = card_elem->li_line;
+                /* exclude any command inside .control ... .endc */
+                if (ciprefix(".control", elem_line)) {
+                    skip_control2++;
+                    continue;
+                }
+                else if (ciprefix(".endc", elem_line)) {
+                    skip_control2--;
+                    continue;
+                }
+                else if (skip_control2 > 0) {
+                    continue;
+                }
+                /* get the element line corresponding to the model */
+                if (*elem_line == inp_get_elem_ident(model_type))
+                {
+                    /* check if correct model type */
+                    int num_terminals = get_number_terminals(elem_line);
+                    if (num_terminals != 0) {
+                        char *elem_model_name = get_model_name(elem_line, num_terminals);
+                        char *model_name = get_subckt_model_name(curr_line);
+                        if (is_a_modelname(elem_model_name))
+                            if (cieq(elem_model_name, model_name))
+                                /* check if element is within scope of model */
+                                if (inp_check_scope_mod(card_elem->level, card->level))
+                                    model_is_in_use = TRUE;
+                    }
+                }
+                else
+                    continue;
+            }
+            if (!model_is_in_use)
+                curr_line[0] = '*';
+        }
+    }
 }
