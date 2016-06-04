aadd0eb3636dd9305326bdf2d2ee8399124bfec8
h_vogt <h_vogt>
2016-06-04 09:49:32 +0200

    inp.c, add .param lines to evaluation of agauss etc.

--- src/frontend/inp.c
+++ src/frontend/inp.c
@@ -1914,6 +1914,7 @@ limit(double nominal_val, double abs_variation)
  * So we have to do the following in each B-line:
  * check for agauss(x,y,z), and replace it by a suitable return value
  * of agauss()
+ * agauss may also occur in .param lines, which have to be treated as well
  */
 
 static void
@@ -1941,7 +1942,7 @@ eval_agauss_bsource(struct line *deck, char *fcn)
             continue;
         }
 
-        if (*curr_line != 'b')
+        if ((*curr_line != 'b') && !ciprefix(".param", curr_line))
             continue;
 
         while ((ap = search_identifier(curr_line, fcn, curr_line)) != NULL) {
