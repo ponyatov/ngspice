44b1df2b8ced886b947a20924bbd995a50e8a5ee
h_vogt <h_vogt>
2016-06-16 23:13:59 +0200

    inp.c, avoid freeing an uninitialized variable

--- src/frontend/inp.c
+++ src/frontend/inp.c
@@ -1630,7 +1630,7 @@ static void inp_savecurrents(struct line *deck, struct line *options, wordlist *
 {
     struct line *tmp_deck, *tmp_line;
     char beg;
-    char *devname, *devline, *newline;
+    char *devname = NULL, *devline, *newline;
     bool goon = FALSE, havesave = FALSE;
     wordlist *tmpword;
 
