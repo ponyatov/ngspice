4b1d52d93a813f797ab1650795ada4f1fc88b7af
rlar <rlar>
2016-06-19 17:50:58 +0200

    whitespace

--- src/include/ngspice/klu-binding.h
+++ src/include/ngspice/klu-binding.h
@@ -1,7 +1,7 @@
 #ifndef _KLU_BINDING_H
 #define _KLU_BINDING_H
 
-#define CREATE_KLU_BINDING_TABLE(ptr, binding, a, b)                                        \
+#define CREATE_KLU_BINDING_TABLE(ptr, binding, a, b)                    \
     if ((here->a != 0) && (here->b != 0)) {                             \
         i = here->ptr ;                                                 \
         matched = (BindElement *) bsearch (&i, BindStruct, nz, sizeof(BindElement), BindCompare) ; \
@@ -9,12 +9,12 @@
         here->ptr = matched->CSC ;                                      \
     }
 
-#define CONVERT_KLU_BINDING_TABLE_TO_COMPLEX(ptr, binding, a, b)                        \
-    if ((here->a != 0) && (here->b != 0))               \
+#define CONVERT_KLU_BINDING_TABLE_TO_COMPLEX(ptr, binding, a, b)        \
+    if ((here->a != 0) && (here->b != 0))                               \
         here->ptr = here->binding->CSC_Complex ;
 
-#define CONVERT_KLU_BINDING_TABLE_TO_REAL(ptr, binding, a, b)                \
-    if ((here->a != 0) && (here->b != 0))       \
+#define CONVERT_KLU_BINDING_TABLE_TO_REAL(ptr, binding, a, b)   \
+    if ((here->a != 0) && (here->b != 0))                       \
         here->ptr = here->binding->CSC ;
 
 #endif
