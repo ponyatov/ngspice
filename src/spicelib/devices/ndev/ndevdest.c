2cc02c73f4d4e581cc42fd9aa273b817494728ee
rlar <rlar>
2016-07-29 22:41:59 +0200

    whitespace for previous commit

--- src/spicelib/devices/ndev/ndevdest.c
+++ src/spicelib/devices/ndev/ndevdest.c
@@ -1,6 +1,6 @@
 /**********
 Permit to use it as your wish.
-Author:	2007 Gong Ding, gdiso@ustc.edu
+Author: 2007 Gong Ding, gdiso@ustc.edu
 University of Science and Technology of China
 **********/
 
