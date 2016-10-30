4d75cf116b685cd1fd090d403350230fe0a1e40b
rlar <rlar>
2016-10-30 11:19:35 +0100

    rename gettok_nc() --> skiptok(), and functionalize it

--- src/frontend/options.c
+++ src/frontend/options.c
@@ -172,7 +172,7 @@ inp_getopts(struct line *deck)
 struct line *
 inp_getoptsc(char *line, struct line *options)
 {
-    gettok_nc(&line);           /* skip option */
+    line = skiptok(line);           /* skip option */
 
     struct line *next = TMALLOC(struct line, 1);
 
