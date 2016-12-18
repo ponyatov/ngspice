e0a2dd06ffc1cf3a722ea294c084d03d69ee06bd
rlar <rlar>
2016-12-18 14:15:25 +0100

    print timing

--- src/spicelib/devices/ind/muttemp.c
+++ src/spicelib/devices/ind/muttemp.c
@@ -236,6 +236,8 @@ MUTtemp(GENmodel *inModel, CKTcircuit *ckt)
             }
 
             static int use_cholesky = 1;
+            static double jactime;
+            double tjactime = SPfrontEnd->IFseconds();
             if (use_cholesky)
                 positive = cholesky(INDmatrix, sz);
             else
@@ -257,6 +259,8 @@ MUTtemp(GENmodel *inModel, CKTcircuit *ckt)
                     }
                 FREE(ev);
             }
+            jactime += SPfrontEnd->IFseconds() - tjactime;
+            fprintf(stderr, "Time used by Jacobi/Cholesky positive definite test:  %6.3g seconds.\n", jactime);
 
             if (!positive) {
                 positive = 1;
