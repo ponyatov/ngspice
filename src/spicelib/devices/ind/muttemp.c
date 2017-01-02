8b77c70bf94356090f6e7fca274a393c47258459
rlar <rlar>
2017-01-02 18:57:07 +0100

    rename local variables

--- src/spicelib/devices/ind/muttemp.c
+++ src/spicelib/devices/ind/muttemp.c
@@ -103,7 +103,7 @@ MUTtemp(GENmodel *inModel, CKTcircuit *ckt)
     MUTmodel *model = (MUTmodel*) inModel;
     MUTinstance *here;
 
-    struct INDsystem *inductanceMatrixSets = NULL;
+    struct INDsystem *first_system = NULL;
 
     NG_IGNORE(ckt);
 
@@ -122,65 +122,65 @@ MUTtemp(GENmodel *inModel, CKTcircuit *ckt)
 
             if (ckt->CKTindverbosity > 0) {
 
-                struct INDsystem *temp;
+                struct INDsystem *system;
 
                 /* Assign 'setIndex' and 'matrixIndex' for L matrix */
                 if (!here->MUTind1->system && !here->MUTind2->system) {
                     /* Create the set */
-                    temp = TMALLOC (struct INDsystem, 1);
-                    temp->size = 2;
-                    temp->next_system = inductanceMatrixSets;
-                    inductanceMatrixSets = temp;
-                    temp->first_ind = here->MUTind1;
+                    system = TMALLOC (struct INDsystem, 1);
+                    system->size = 2;
+                    system->next_system = first_system;
+                    first_system = system;
+                    system->first_ind = here->MUTind1;
                     here->MUTind1->system_next_ind = here->MUTind2;
                     here->MUTind2->system_next_ind = NULL;
-                    here->MUTind1->system = temp;
-                    here->MUTind2->system = temp;
-                    temp->first_mut = here;
+                    here->MUTind1->system = system;
+                    here->MUTind2->system = system;
+                    system->first_mut = here;
                     here->system_next_mut = NULL;
                 } else if (here->MUTind1->system && !here->MUTind2->system) {
                     /* Add the new MUTind2 into the set */
-                    temp = here->MUTind1->system;
-                    temp->size++;
-                    here->MUTind2->system_next_ind = temp->first_ind;
-                    temp->first_ind = here->MUTind2;
-                    here->system_next_mut = temp->first_mut;
-                    temp->first_mut = here;
-                    here->MUTind2->system = temp;
+                    system = here->MUTind1->system;
+                    system->size++;
+                    here->MUTind2->system_next_ind = system->first_ind;
+                    system->first_ind = here->MUTind2;
+                    here->system_next_mut = system->first_mut;
+                    system->first_mut = here;
+                    here->MUTind2->system = system;
                 } else if (!here->MUTind1->system && here->MUTind2->system) {
                     /* Add the new MUTind1 into the set */
-                    temp = here->MUTind2->system;
-                    temp->size++;
-                    here->MUTind1->system_next_ind = temp->first_ind;
-                    temp->first_ind = here->MUTind1;
-                    here->system_next_mut = temp->first_mut;
-                    temp->first_mut = here;
-                    here->MUTind1->system = temp;
+                    system = here->MUTind2->system;
+                    system->size++;
+                    here->MUTind1->system_next_ind = system->first_ind;
+                    system->first_ind = here->MUTind1;
+                    here->system_next_mut = system->first_mut;
+                    system->first_mut = here;
+                    here->MUTind1->system = system;
                 } else if (here->MUTind1->system == here->MUTind2->system) {
                     /* Add only the K coefficient into the set */
-                    temp = here->MUTind2->system;
-                    here->system_next_mut = temp->first_mut;
-                    temp->first_mut = here;
+                    system = here->MUTind2->system;
+                    here->system_next_mut = system->first_mut;
+                    system->first_mut = here;
                 } else {
                     struct INDsystem *s1 = here->MUTind1->system;
                     struct INDsystem *s2 = here->MUTind2->system;
-                    MUTinstance *hm;
-                    INDinstance *hi;
+                    MUTinstance *mut;
+                    INDinstance *ind;
                     // append set2 to set1, leave a consumed set2 behind
                     s1->size += s2->size;
                     s2->size = 0;
-                    for (hi = s2->first_ind; hi; hi = hi->system_next_ind) {
-                        hi->system = s1;
-                        if (!hi->system_next_ind)
+                    for (ind = s2->first_ind; ind; ind = ind->system_next_ind) {
+                        ind->system = s1;
+                        if (!ind->system_next_ind)
                             break;
                     }
-                    hi->system_next_ind = s1->first_ind;
+                    ind->system_next_ind = s1->first_ind;
                     s1->first_ind = s2->first_ind;
                     s2->first_ind = NULL;
-                    for (hm = s2->first_mut; hm; hm = hm->system_next_mut)
-                        if (!hm->system_next_mut)
+                    for (mut = s2->first_mut; mut; mut = mut->system_next_mut)
+                        if (!mut->system_next_mut)
                             break;
-                    hm->system_next_mut = s1->first_mut;
+                    mut->system_next_mut = s1->first_mut;
                     here->system_next_mut = s2->first_mut;
                     s1->first_mut = here;
                     s2->first_mut = NULL;
@@ -188,40 +188,40 @@ MUTtemp(GENmodel *inModel, CKTcircuit *ckt)
             }
         }
 
-    if (inductanceMatrixSets) {
-        struct INDsystem *temp;
+    if (first_system) {
+        struct INDsystem *system;
         int sz = 0;
 
-        for (temp = inductanceMatrixSets; temp; temp = temp->next_system)
-            if (sz < temp->size)
-                sz = temp->size;
+        for (system = first_system; system; system = system->next_system)
+            if (sz < system->size)
+                sz = system->size;
 
         char *pop = TMALLOC(char, sz * sz);
         double *INDmatrix = TMALLOC(double, sz * sz);
 
-        for (temp = inductanceMatrixSets; temp; temp = temp->next_system) {
-            if (!temp->size)
+        for (system = first_system; system; system = system->next_system) {
+            if (!system->size)
                 continue;
 
             int positive, i;
 
-            sz = temp->size;
+            sz = system->size;
 
             memset(pop, 0, (size_t)(sz*sz));
             memset(INDmatrix, 0, (size_t)(sz*sz) * sizeof(double));
 
-            INDinstance *hi = temp->first_ind;
-            for (i = 0; hi; hi = hi->system_next_ind) {
-                INDmatrix [i * sz + i] = hi->INDinduct;
-                hi->system_idx = i++;
+            INDinstance *ind = system->first_ind;
+            for (i = 0; ind; ind = ind->system_next_ind) {
+                INDmatrix [i * sz + i] = ind->INDinduct;
+                ind->system_idx = i++;
             }
 
-            MUTinstance *hm = temp->first_mut;
+            MUTinstance *mut = system->first_mut;
             int expect = (sz*sz - sz) / 2;
             int repetitions = 0;
-            for (; hm; hm = hm->system_next_mut) {
-                int j = hm->MUTind1->system_idx;
-                int k = hm->MUTind2->system_idx;
+            for (; mut; mut = mut->system_next_mut) {
+                int j = mut->MUTind1->system_idx;
+                int k = mut->MUTind2->system_idx;
                 if (j < k)
                     SWAP(int, j, k);
                 if (pop[j*sz + k]) {
@@ -230,7 +230,7 @@ MUTtemp(GENmodel *inModel, CKTcircuit *ckt)
                     pop[j*sz + k] = 1;
                     expect --;
                 }
-                INDmatrix [j * sz + k] = INDmatrix [k * sz + j] = hm->MUTfactor;
+                INDmatrix [j * sz + k] = INDmatrix [k * sz + j] = mut->MUTfactor;
             }
 
             static int use_cholesky = 1;
@@ -263,13 +263,13 @@ MUTtemp(GENmodel *inModel, CKTcircuit *ckt)
             if (!positive) {
                 positive = 1;
                 /* ignore check if all |K| == 1 and all L >= 0 */
-                for (hm = temp->first_mut; hm; hm = hm->system_next_mut)
-                    if (fabs(hm->MUTcoupling) != 1.0) {
+                for (mut = system->first_mut; mut; mut = mut->system_next_mut)
+                    if (fabs(mut->MUTcoupling) != 1.0) {
                         positive = 0;
                         break;
                     }
-                for (hi = temp->first_ind; hi; hi = hi->system_next_ind)
-                    if (hi->INDinduct < 0) {
+                for (ind = system->first_ind; ind; ind = ind->system_next_ind)
+                    if (ind->INDinduct < 0) {
                         positive = 0;
                         break;
                     }
@@ -277,20 +277,20 @@ MUTtemp(GENmodel *inModel, CKTcircuit *ckt)
 
             if (!positive || repetitions || (expect && ckt->CKTindverbosity > 1)) {
                 fprintf(stderr, "The Inductive System consisting of\n");
-                for (hi = temp->first_ind; hi; hi = hi->system_next_ind)
-                    fprintf(stderr, " %s", hi->INDname);
+                for (ind = system->first_ind; ind; ind = ind->system_next_ind)
+                    fprintf(stderr, " %s", ind->INDname);
                 fprintf(stderr, "\n");
-                for (hm = temp->first_mut; hm; hm = hm->system_next_mut)
-                    fprintf(stderr, " %s", hm->MUTname);
+                for (mut = system->first_mut; mut; mut = mut->system_next_mut)
+                    fprintf(stderr, " %s", mut->MUTname);
                 fprintf(stderr, "\n");
                 if (!positive)
                     fprintf(stderr, "is not positive definite\n");
-                for (hm = temp->first_mut; hm; hm = hm->system_next_mut)
-                    if (fabs(hm->MUTcoupling) > 1.0)
-                        fprintf(stderr, " |%s| > 1\n", hm->MUTname);
-                for (hi = temp->first_ind; hi; hi = hi->system_next_ind)
-                    if (hi->INDinduct < 0)
-                        fprintf(stderr, " %s < 0\n", hi->INDname);
+                for (mut = system->first_mut; mut; mut = mut->system_next_mut)
+                    if (fabs(mut->MUTcoupling) > 1.0)
+                        fprintf(stderr, " |%s| > 1\n", mut->MUTname);
+                for (ind = system->first_ind; ind; ind = ind->system_next_ind)
+                    if (ind->INDinduct < 0)
+                        fprintf(stderr, " %s < 0\n", ind->INDname);
                 if (repetitions)
                     fprintf(stderr, "has dupplicate K instances\n");
                 if (expect && ckt->CKTindverbosity > 1)
@@ -302,10 +302,10 @@ MUTtemp(GENmodel *inModel, CKTcircuit *ckt)
         tfree(pop);
         tfree(INDmatrix);
 
-        for (temp = inductanceMatrixSets; temp;) {
-            struct INDsystem *next_system = temp->next_system;
-            tfree(temp);
-            temp = next_system;
+        for (system = first_system; system;) {
+            struct INDsystem *next_system = system->next_system;
+            tfree(system);
+            system = next_system;
         }
     }
 
