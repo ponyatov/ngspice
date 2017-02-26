50e53b3ae9819c2cac32615607a870292e368a97
h_vogt <h_vogt>
2017-02-26 11:42:25 +0100

    runcoms.c, print more info after 'ASCII raw file' etc.,

--- src/frontend/runcoms.c
+++ src/frontend/runcoms.c
@@ -273,7 +273,7 @@ dosim(
                     tfree(nname);
                     return 1;
                 }
-                fprintf(cp_out, "ASCII raw file\n");
+                fprintf(cp_out, "ASCII raw file %s\n", nname);
             }
             else {
                 if ((rawfileFp = fopen(nname, "wb")) == NULL) {
@@ -282,7 +282,7 @@ dosim(
                     tfree(nname);
                     return 1;
                 }
-                fprintf(cp_out, "binary raw file\n");
+                fprintf(cp_out, "binary raw file %s\n", nname);
             }
 /*---------------------------------------------------------------------------*/
 #else
