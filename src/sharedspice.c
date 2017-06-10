2f58199f8019ba0e92ee0a96b9f275c264db67c8
h_vogt <h_vogt>
2017-06-10 14:14:02 +0200

    complete type definition of SendEvtData, SendInitEvtData enable usage of wantevtdata

--- src/sharedspice.c
+++ src/sharedspice.c
@@ -10,7 +10,7 @@
 /*******************/
 
 #ifdef _MSC_VER
-#define SHAREDSPICE_version "25.1"
+#define SHAREDSPICE_version "26.2"
 #define STDIN_FILENO    0
 #define STDOUT_FILENO   1
 #define STDERR_FILENO   2
@@ -234,6 +234,9 @@ static bool coquit = FALSE;
 static jmp_buf errbufm, errbufc;
 static int intermj = 1;
 bool wantevtdata = FALSE;
+static SendInitEvtData* sendinitevt;
+static SendEvtData* sendevt;
+static void* euserptr;
 
 
 // thread IDs
@@ -602,7 +605,7 @@ ngSpice_Init(SendChar* printfcn, SendStat* statusfcn, ControlledExit* ngspiceexi
     if (!bgtr)
         nobgtrwanted = TRUE;
     immediate = FALSE;
-    wantevtdata = TRUE;
+
 #ifdef THREADS
     /* init the mutexes */
 #ifdef HAVE_LIBPTHREAD
@@ -970,6 +973,11 @@ int  ngSpice_Init_Evt(SendEvtData* sevtdata, SendInitEvtData* sinitevtdata, void
 {
     if (sevtdata)
         wantevtdata = TRUE;
+    else
+        wantevtdata = FALSE;
+    sendinitevt = sinitevtdata;
+    sendevt = sevtdata;
+    euserptr = userData;
     return(TRUE);
 }
 
@@ -1972,12 +1980,14 @@ sharedsync(double *pckttime, double *pcktdelta, double olddelta, double finalt,
 
 void shared_send_event(int index, double step, double dvalue, char *svalue, void *pvalue, int plen, int mode)
 {
-    printf("");
+    if(wantevtdata)
+        sendevt(index, step, dvalue, svalue, pvalue, plen, mode, euserptr);
     return;
 }
 
 void shared_send_dict(char* dictline)
 {
-    printf("%s", dictline);
+    if (sendinitevt)
+        sendinitevt(dictline, euserptr);
 }
 
