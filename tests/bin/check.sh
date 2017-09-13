cbe22b36b0f3c2ce8f3ce2be96513a0e1cf8d1bd
rlar <rlar>
2017-09-13 18:55:07 +0200

    tests/bin/check.sh, process MINGW and MSYS test results like Linux

--- tests/bin/check.sh
+++ tests/bin/check.sh
@@ -25,19 +25,7 @@ testdir=`dirname $TEST`
 HOST_TYPE=`uname -srvm`
 
 case $HOST_TYPE in
-    MINGW*|MSYS*)
-      $SPICE --batch $testdir/$testname.cir -o $testname.test &&\
-      sed -e 's/e-000/e+000/g' $testname.test | sed 's/e-0/e-/g' | sed 's/e+0/e+/g' | egrep -v "$FILTER" > $testname.test_tmp &&\
-      sed -e 's/-0$/ 0/g' $testdir/$testname.out | egrep -v "$FILTER" > $testname.out_tmp
-      if diff -B -w -u $testname.out_tmp $testname.test_tmp; then
-          rm $testname.test $testname.test_tmp $testname.out_tmp
-          exit 0
-      fi
-      rm -f $testname.test_tmp $testname.out_tmp
-      sed -e 's/e-000/e+000/g' $testname.test | sed 's/e-0/e-/g' | sed 's/e+0/e+/g' > $testname.test_tmp
-      mv $testname.test_tmp $testname.test
-      ;;
-    Linux*|Darwin*|CYGWIN*)
+    Linux*|Darwin*|CYGWIN*|MINGW*|MSYS*)
       $SPICE --batch $testdir/$testname.cir >$testname.test &&\
       egrep -v "$FILTER" $testname.test > $testname.test_tmp &&\
       egrep -v "$FILTER" $testdir/$testname.out > $testname.out_tmp
